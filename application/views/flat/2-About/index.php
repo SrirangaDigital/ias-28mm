460a165e3710799c009c647893f3c9ad8f0b8abe
shivasdst <shivably04sdst@gmail.com>
2017-06-01 14:30:44 +0530

    updated about page

--- application/views/flat/2-About/index.php
+++ application/views/flat/2-About/index.php
@@ -18,13 +18,13 @@
     Known for its diversity in scientific content delivered by highly acclaimed researchers, the Mid-Year meeting of the Indian Academy of Sciences is a festival celebrating scientific accomplishments from across India. The Mid-Year Meetings – shorter and more intense than the Annual Meetings, but of the same ‘status’– continue to serve as a platform for the newly elected Fellows and Associates to present their inaugural talk covering the most recent of their scientific achievements. 
 </p>
 <p>
-    This year, the 28<sup>th</sup> Mid-Year meeting will be held on the 30<sup>th</sup> of June and the 1st of July at the Faculty Hall of the Indian Institute of Science. Fellows of the Academy as well as Honorary Fellows, Associates, invited teachers and students will come together for a day and a half, participating in an exchange of knowledge and ideas. With talks on subjects ranging from caretaker tumour suppressors to quantum matter and nanomaterials-based light-harvesting systems to the unique properties of DNA replication in pathogens, the 28<sup>th</sup> Mid-Year meeting holds promise of an exciting occasion of learning exchange – an event one cannot afford to miss. 
+    This year, the 28<sup>th</sup> Mid-Year meeting will be held on the 30<sup>th</sup> of June and the 1<sup>st</sup> of July at the Faculty Hall of the Indian Institute of Science. Fellows of the Academy as well as Honorary Fellows, Associates, invited teachers and students will come together for a day and a half, participating in an exchange of knowledge and ideas. With talks on subjects ranging from caretaker tumour suppressors to quantum matter and nanomaterials-based light-harvesting systems to the unique properties of DNA replication in pathogens, the 28<sup>th</sup> Mid-Year meeting holds promise of an exciting occasion of learning exchange – an event one cannot afford to miss. 
 </p>
 <p>
     The highlights of this meeting are the special lectures: Gagandeep Kang will speak on ‘Vaccines and Public Health in India’, highlighting the challenges India faces in the development and implementation of vaccines. Ullas Karanth, using examples from his three decades of ecological research on India’s national animal – the tiger, will explore possible solutions for the challenges that ‘Conservation Science at Macro-Ecological Scales’ face in India.
 </p>
 <p>
-    The public lecture, to be held in the evening of the 30<sup>th</sup> June, will be on ‘C. Subramania Bharati: Nationalism and Creativity’ by A.R. Venkatachalapathy. A symposium on Molecular Machines will be held on the 1st, which will provide insights into this fascinating field from the perspectives of physics, cell biology, structural biology and chemistry. Enough said.
+    The public lecture, to be held in the evening of the 30<sup>th</sup> June, will be on ‘C. Subramania Bharati: Nationalism and Creativity’ by A.R. Venkatachalapathy. A symposium on Molecular Machines will be held on the 1<sup>st</sup>, which will provide insights into this fascinating field from the perspectives of physics, cell biology, structural biology and chemistry. Enough said.
 </p>
 <p>
     See you there!
