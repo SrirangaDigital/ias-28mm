13d20ac715ab0746fc7374ad191986d6195a9a7b
shivasdst <shivably04sdst@gmail.com>
2017-06-10 12:49:07 +0530

    updated general sidebar

--- application/views/generalSidebar.php
+++ application/views/generalSidebar.php
@@ -2,16 +2,17 @@
 
     <h1>Schedule</h1>
     <ul>
-        <li><a href="#" target="_blank">Day1</a></li>
-        <li><a href="#" target="_blank">Day2</a></li>
+        <li><a href="<?=BASE_URL?>page/flat/Schedule#day1" target="_blank">Day1</a></li>
+        <li><a href="<?=BASE_URL?>page/flat/Schedule#day2" target="_blank">Day2</a></li>
     </ul>            
 
     <h1>For Participants</h1>
     <ul>
         <!-- <li><a href="http://web-japps.ias.ac.in:8080/Annualmeeting2016/annual/register/Register.jsp" target="_blank">Registration</a></li> -->
-        <li><a href="#" target="_blank">Venue</a></li>
+        <li><a href="<?=BASE_URL?>Information/Venue" target="_blank">Venue</a></li>
         <li><a href="#" target="_blank">Accomodation</a></li>
-        <li><a href="#" target="_blank">Transport</a></li>
+        <li><a href="<?=BASE_URL?>Information/Transport" target="_blank">Transport</a></li>
+        <li><a href="<?=BASE_URL?>Information/Press_and_Media" target="_blank">Press and Media</a></li>
         <li><a href="<?=BASE_URL?>page/flat/Information/Contact" target="_blank">Contact</a></li>
         <li><a href="#" target="_blank">Other Information</a></li>
     </ul>    
