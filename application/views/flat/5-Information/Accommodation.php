2e6c40916be08075b745709a903879119d83a8da
shivasdst <shivably04sdst@gmail.com>
2017-06-13 15:36:48 +0530

    updated accommodation details

--- application/views/flat/5-Information/Accommodation.php
+++ application/views/flat/5-Information/Accommodation.php
@@ -10,12 +10,27 @@
     <div class="row">
         <div class="col-md-9 main">
             <h1>Accommodation</h1>
-            <p>Arrangements are made to accommodate all the registered participants. Details regarding this will be sent to the registered participants. Volunteers at the airport will assist the participants in reaching their respective places of accommodation.</p>
+            <p>The Academy has made arrangements to accommodate all participants who have requested for it. Details regarding this will be sent to the registered participants. Volunteers at the airport will assist the participants in reaching their respective places of accommodation.</p>
             <p>For any queries regarding accommodation, please contact</p>
-            <ul class="general-list">
-                <li><strong>Venkatarathnam, N.</strong><br />+91-90080 32003<br />venkat@ias.ac.in</li>
-            </ul>
-            <p>Transportation between the guest house and meeting venue will be provided. Breakfast will be served to the participants by 07.30 –8:30 AM at the respective hotels, while lunch, on both days, and dinner, on the 30th of June, will be served at the meeting venue. Transportation to the hotels will be made soon after the dinner.</p> 
+
+            <div class="row">
+                <div class="col-md-4">
+                    <strong>N. Maheshchandra</strong><br />
+                    Executive Secretary<br />
+                    Indian Academy of Sciences<br /> 
+                    Off. Tel.: 080 2266 1203<br />
+                    Mob: 09008032004<br />
+                    Fax: 080 2361 6094<br />
+                    Email: execsec@ias.ac.in
+                </div>
+                <div class="col-md-4">
+                    <strong>Venkatarathnam, N.</strong><br />
+                    +91-90080 32003<br />
+                    Email: venkat@ias.ac.in
+                </div>
+            </div>
+            <br />
+            <p>Transportation between the guest house and meeting venue will be provided. Breakfast will be served to the participants by 07.30 –8:30 AM at the respective places of accommodation, while lunch, on both days, and dinner, on the 30th of June, will be served at the IISc main guest house lawn. Transportation to the places of accommodation will be made soon after the dinner.</p> 
         </div>
         <div class="col-md-3 sidebar">
 <?php require_once('application/views/generalSidebar.php');?>
